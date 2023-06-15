8caae0bfa2c43b110555a0110409090aa69f8670
abdalghani05 <50181687+abdalghani05@users.noreply.github.com>
2023-06-15 10:53:58 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 * main - A program that prints a line with puts funstion
 *return : 0 (success)
 */
-int main(void) {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+int main(void) 
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
