edd4b11f93b8d2ad169264b0374b1d25635fc1ab
abdalghani05 <50181687+abdalghani05@users.noreply.github.com>
2023-06-15 11:17:16 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
-*main - Entry point
-*Return: 1
-*/
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
