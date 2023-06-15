6bfb56e10c91bb9148a2c6b9febefec8535f2209
abdalghani05 <50181687+abdalghani05@users.noreply.github.com>
2023-06-15 10:59:00 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry of the program
+ * Return: Always returns (0)
+ */
 
-int main() {
-    printf("A piece of art,\n");
-    printf("but the outcome is a masterpiece!\n");
-    return 0;
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
